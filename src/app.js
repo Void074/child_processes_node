3c3e890841bdfcf3035d217f4c90cd396a901366
Gedare Dorke <ggedare074@protonmail.com>
2024-05-10 06:30:27 +1000

    feat (src/lib/simulate_database_backup): logs can now be created by date (src/app.js): passing project root to bash script in src/lib/simulate_database_backup (src/log_check): generates date which is passed to bash script

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import { spawn } from 'node:child_process'
 import config from './config/app.config.js'
+import path from 'path'
+
+const root = path.resolve()
+process.env.DIR_PATH = root
 
 //TODO(Gedare): explore execFile and Fork
 //TODO(Gedare): see if I can setuid for this process
